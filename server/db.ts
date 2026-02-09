f5c12cb46563e2dfebd32cad8a9d39091d385de5
mkukhalil <mkukhalil1@gmail.com>
2026-02-09 09:33:30 +0500

    deleted unnecessary files

--- server/db.ts
+++
@@ -1,9 +0,0 @@
-import { drizzle } from "drizzle-orm/node-postgres";
-import pg from "pg";
-import * as schema from "@shared/schema";
-
-const { Pool } = pg;
-
-// No database connection for this project
-export const pool = new Pool({ connectionString: "postgres://dummy" });
-export const db = drizzle(pool, { schema });
